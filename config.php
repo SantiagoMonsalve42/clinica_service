fc4a52bbe8608fd0c3325212730ec0667b960e70
SantiagoMonsalve42 <santi.monsalve09@gmail.com>
2020-11-28 01:00:26 -0500

    Esquema general web service

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+function conection(){
+    $host="localhost";
+    $user="root";
+    $pass="123456";
+    $bdname="clinica";
+    $RUTA="http://localhost.com/";
+    $link=mysqli_connect($host,$user,$pass)
+    or die("Error 1 del tipo: ".mysqli_error($link));
+
+    mysqli_select_db($link,$bdname)
+    or die("Error 2 del tipo:".mysqli_error($link));
+    return $link;
+}
+
+?>
