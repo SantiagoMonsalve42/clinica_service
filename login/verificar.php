37ece698b58cc38fae083a5d82bc80993868d7d8
santiago monsalve <santi.monsalve09@gmail.com>
2021-02-12 20:20:14 -0500

    login bn

--- login/verificar.php
+++ login/verificar.php
@@ -5,22 +5,7 @@
 	 $objf = new facade();
 	 $array;
 	 $id= $objf->validarCredencial($_POST["correo"],$_POST["clave"]);
-		switch($id){
-			case 0:
-				$array = array('id' => 0, 'variable_sesion' => "null");
-				break;
-			case 1:
-				$array = array('id' => 1, 'variable_sesion' => $_POST["correo"]);
-				break;
-			case 2:
-				$array = array('id' => 2, 'variable_sesion' => $_POST["correo"]);
-				break;
-			case 3:
-				$array = array('id' => 3, 'variable_sesion' => $_POST["correo"]);
-				break;
-		}
-		echo json_encode($array);
-
+     echo $id;
 	}
 
 ?>
